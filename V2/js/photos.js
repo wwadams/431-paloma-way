82ee27453b68dc51e3040107781d9b2c971847ae
wwadams <wwadams@aol.com>
2026-08-07 09:05:52 -0600

    Create galleryData for home image categories
    Added gallery data for various home categories including Exterior, Kitchen, Living Room, Primary Bedroom, and Bathroom.

---
+++ V2/js/photos.js
@@ -0,0 +1,47 @@
+const galleryData = [
+    {
+        category: "Exterior",
+        file: "images/exterior/front.jpg",
+        title: "Front of Home",
+        featured: true
+    },
+    {
+        category: "Exterior",
+        file: "images/exterior/front-angle.jpg",
+        title: "Front Corner View"
+    },
+    {
+        category: "Exterior",
+        file: "images/exterior/backyard.jpg",
+        title: "Back Yard"
+    },
+
+    {
+        category: "Kitchen",
+        file: "images/kitchen/kitchen-01.jpg",
+        title: "Kitchen"
+    },
+    {
+        category: "Kitchen",
+        file: "images/kitchen/kitchen-02.jpg",
+        title: "Island and Breakfast Bar"
+    },
+
+    {
+        category: "Living Room",
+        file: "images/living/living-01.jpg",
+        title: "Living Room"
+    },
+
+    {
+        category: "Primary Bedroom",
+        file: "images/primary/primary-01.jpg",
+        title: "Primary Bedroom"
+    },
+
+    {
+        category: "Bathroom",
+        file: "images/bathrooms/bath-01.jpg",
+        title: "Primary Bathroom"
+    }
+];
