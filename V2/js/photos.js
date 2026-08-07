5a4b05c8ab1e5cfd6aab61b04f31fee816edca40
wwadams <wwadams@aol.com>
2026-08-07 14:30:52 -0600

    Add width and height to photo gallery items
    Added width and height properties to gallery items.

--- V2/js/photos.js
+++ V2/js/photos.js
@@ -2,46 +2,62 @@ const galleryData = [
     {
         category: "Exterior",
         file: "images/exterior/6079.jpg",
+        width: 480,
+        height: 360,
         title: "Front of Home",
         featured: true
     },
     {
         category: "Exterior",
         file: "images/exterior/6081.jpg",
+        width: 360,
+        height: 480,
         title: "Front Corner View"
     },
     {
         category: "Exterior",
         file: "images/exterior/6097.jpg",
+        width: 360,
+        height: 480,
         title: "Back Yard"
     },
 
     {
         category: "Kitchen",
         file: "images/kitchen/kitchen-01.jpg",
+        width: 360,
+        height: 480,
         title: "Kitchen"
     },
     {
         category: "Kitchen",
         file: "images/kitchen/kitchen-02.jpg",
+        width: 360,
+        height: 480,
         title: "Island and Breakfast Bar"
     },
 
     {
         category: "Living Room",
         file: "images/living/living-01.jpg",
+        width: 360,
+        height: 480,
         title: "Living Room"
     },
 
     {
         category: "Primary Bedroom",
         file: "images/primary/primary-01.jpg",
+        width: 360,
+        height: 480,
         title: "Primary Bedroom"
     },
 
     {
         category: "Bathroom",
         file: "images/bathrooms/bath-01.jpg",
+        width: 360,
+        height: 480,
         title: "Primary Bathroom"
     }
 ];
